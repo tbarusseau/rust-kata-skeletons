d020eaef20eab4e06197302c193bf621c46def4b
Thomas BARUSSEAU <thomas.barusseau@thetribe.io>
2020-11-02 22:18:19 +0100

    Add 04_generic_types.rs

---
+++ src/bin/04_generic_types.rs
@@ -0,0 +1,25 @@
+// Seen in this chapter:
+// - Generic types in structs, enums
+// - Option :D
+// - Result :D!
+// - Error handling?!
+// - Unwrapping
+// - Vectors
+
+// That's a lot!
+
+// Exercise: create a generic struct that holds a type,
+// and an enum that contains several different generic types.
+// And... That's it. We're still lacking important stuff to write
+// interesting code with generic types.
+
+// Exercise: create a function that takes a filename (`String`), removes a
+// suffix (`- Copy`), and returns the filename if the suffix was found, else
+// an error. Yes, we'll return a `Result` :D
+
+// Exercise: use the previous function, and print the result after being unwrapped!
+
+// Exercise: use the previous function, print the result after using the `?` operator
+// from the `main` function. It means that we'll need to update the `main` function's prototype!
+
+fn main() {}
