9752373241e8782efb2725734cc60a9b2c9b3ded
Thomas BARUSSEAU <thomas.barusseau@thetribe.io>
2020-11-02 21:11:30 +0100

    Add 01_basics.rs

---
+++ src/bin/01_basics.rs
@@ -0,0 +1,9 @@
+// Functions to create:
+// - `add_one`, which takes a number and adds one to it
+// - `double_n_times`, which doubles a number `n` times
+// - `multiply_by_pi`, which multiplies a number by the PI constant
+// - Create a function that returns an array of `n` elements, whatever you want
+
+// use std::f64::consts::PI;
+
+fn main() {}
