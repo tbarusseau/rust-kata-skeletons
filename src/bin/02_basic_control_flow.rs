540fff946a47a0fe3375900b0cdd05d5a99dedf3
Thomas BARUSSEAU <thomas.barusseau@thetribe.io>
2020-11-02 21:21:18 +0100

    Some changes + 02_basic_control_flow.rs

---
+++ src/bin/02_basic_control_flow.rs
@@ -0,0 +1,17 @@
+// Seen in the second chapter:
+// - if/else if/else
+// - loop, while, for
+// - match
+
+// This time, we'll create a FizzBuzz function:
+// - Iterate over numbers from 0 to 100
+// - If the number is divisible by 3, print `Fizz`.
+// - If the number is divisible by 5, print `Buzz`.
+//   This works with the previous rule if the number is both divisible by 3 and 5!
+// - If the number is neither divisible by 3 nor 5, print the number.
+
+// fn fizzbuzz() {}
+
+fn main() {
+    // fizzbuzz();
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::fs::read_dir;
 
 fn main() -> Result<(), ()> {
     let r = read_dir("./src/bin").expect("Couldn't find ./src/bin directory");
+
+    println!("Usage: cargo run --bin <bin>");
+    println!("Available binaries:");
+
     r.into_iter().for_each(|e| {
-        println!("Usage: cargo run --bin <bin>");
-        println!("Available binaries:");
         println!(
             "$ cargo run --bin {}",
             e.unwrap()
