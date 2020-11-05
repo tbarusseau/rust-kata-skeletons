d01b178ae48ca6ab89acfccae0ecb171cf596bba
Thomas BARUSSEAU <thomas.barusseau@thetribe.io>
2020-11-05 21:49:21 +0100

    Add (uninspired) 06_text.rs

---
+++ src/bin/06_text.rs
@@ -0,0 +1,11 @@
+// Seen in this chapter:
+// - String literals (regular, multi-line, raw)
+// - A short lesson about utf-8
+// - Character escaping
+// - String slices
+// - Strings
+// - Some methods: `push_str`, `replace`, `to_lowercase`/`uppercase`, `trim`, `concat`, `join`
+// - The `format!` macro
+// - `to_string()`
+
+fn main() {}
