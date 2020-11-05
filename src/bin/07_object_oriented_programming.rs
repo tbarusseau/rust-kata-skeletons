4af5eb49833fe7338c92116737631f17cb01832e
Thomas BARUSSEAU <thomas.barusseau@thetribe.io>
2020-11-05 22:07:02 +0100

    Add (completely uninspired) 07_object_oriented_programming.rs

---
+++ src/bin/07_object_oriented_programming.rs
@@ -0,0 +1,15 @@
+// Seen in this chapter:
+// - Rust is not OOP! No "class" inheritance
+// - Methods implementations
+// - Traits
+// - Trait inheritance
+// - Static and dynamic dispatch (+ trait objects)
+// - Parameterized generic types
+// - Box
+
+// Exercise: create a `Add` trait, which adds itself to a parameter.
+// Exercise: `impl` the `Add` trait to a number type, for example i32.
+
+// Not really inspired: Rustlings?
+
+fn main() {}
