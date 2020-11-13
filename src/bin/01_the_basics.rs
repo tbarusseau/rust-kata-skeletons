871f7de7c440b85e70f14ae1ecb3a8483b2c06a9
ludovic geoffroy <ludovic.geoffroy@thetribe.io>
2020-11-13 11:41:08 +0100

    01 - the basics

--- src/bin/01_the_basics.rs
+++ src/bin/01_the_basics.rs
@@ -4,6 +4,25 @@
 // - `multiply_by_pi`, which multiplies a number by the PI constant
 // - Create a function that returns an array of `n` elements, whatever you want
 
-// use std::f64::consts::PI;
+use std::f64::consts::PI;
 
-fn main() {}
+fn add_one(x: i32) -> i32 {
+    return x + 1;
+}
+
+fn double_n_times(x: i32, n: u32) -> i32 {
+    let base = 2i32;
+    return x * base.pow(n);
+}
+
+fn multiply_by_pi(x: f64) -> f64 {
+    return PI * x;
+}
+
+fn trois_array() -> [i32; 3] {
+    return [1, 2, 3];
+}
+
+fn main() {
+    println!("{}, {}, {}, {:?}", add_one(12), double_n_times(12, 3), multiply_by_pi(12f64), trois_array());
+}
