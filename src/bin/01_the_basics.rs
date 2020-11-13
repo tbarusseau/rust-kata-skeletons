dcaee9530378bd216f4ebc486273ecabbdc938c2
Maeevick <sayag_vs@hotmail.fr>
2020-11-13 12:17:07 +0100

    ex1

--- src/bin/01_the_basics.rs
+++ src/bin/01_the_basics.rs
@@ -4,6 +4,43 @@
 // - `multiply_by_pi`, which multiplies a number by the PI constant
 // - Create a function that returns an array of `n` elements, whatever you want
 
-// use std::f64::consts::PI;
+use std::f64::consts::PI;
 
-fn main() {}
+fn main() {
+    println!("expect add_one(4) to be 5, result is : {}", add_one(4) == 4 + 1);
+    println!("expect double_n_times(2, 3) to be 8, result is : {}", double_n_times(2, 3) == 2*2*2);
+    println!("expect multiply_by_pi(2) to be ~3.28..., result is : {}", multiply_by_pi(2.0) == 2.0 * PI);
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn double_n_times(x: i32, n: u32) -> i32 {
+    x.pow(n)
+}
+
+fn multiply_by_pi(y: f64) -> f64 {
+    y * PI
+}
+
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn should_increment_the_number_by_one() {
+        assert_eq!(add_one(4), 4 + 1);
+    }
+
+    #[test]
+    fn should_multiply_the_number_n_times() {
+        assert_eq!(double_n_times(8, 3), 8 * 8 * 8);
+    }
+
+    #[test]
+    fn should_multiply_the_number_by_pi_constant() {
+        assert_eq!(multiply_by_pi(44.0), 44.0 * PI);
+    }
+}
+
+
