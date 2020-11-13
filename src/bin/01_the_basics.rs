ee41600e973da71afae51a95c50bc1480e0d2a1e
Alexandre Cariou <alexandre.cariou@thetribe.io>
2020-11-13 12:25:55 +0100

    day 1

--- src/bin/01_the_basics.rs
+++ src/bin/01_the_basics.rs
@@ -6,4 +6,15 @@
 
 // use std::f64::consts::PI;
 
-fn main() {}
+fn add_one(number: i32) -> i32 {
+    return number + 1;
+}
+
+fn double_n_times(number: i32, power: u32) -> i32 {
+    return number.pow(power);
+}
+
+fn main() {
+    print!("{}", add_one(12));
+    print!("\n{}", double_n_times(4, 4));
+}
