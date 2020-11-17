960d4539000d00cd5ffa0eb2b5eb7ec22a037e04
Pierre Lavalley <pierre.lavalley@thetribe.io>
2020-11-17 11:45:31 +0100

    Exo 2

--- src/bin/02_basic_control_flow.rs
+++ src/bin/02_basic_control_flow.rs
@@ -11,8 +11,34 @@
 // - If the number is neither divisible by 3 nor 5, print the number.
 // When it works, we'll create a FizzBuzzJazz function :D
 
-// fn fizzbuzz() {}
+fn fizzbuzzWithIf() {
+    for x in 0..=100 {
+        if x % 3 == 0 && x % 5 == 0 {
+            println!("FizzBuzz");
+        }
+        else if x % 3 == 0 {
+            println!("Fizz");
+        }
+        else if x % 5 == 0 {
+            println!("Buzz");
+        }
+        else {
+            println!("{}", x);
+        }
+    }
+}
+
+fn fizzbuzz() {
+    for x in 0..=100 {
+        match (x % 3, x % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{}", x),
+        }
+    }
+}
 
 fn main() {
-    // fizzbuzz();
+    fizzbuzz();
 }
