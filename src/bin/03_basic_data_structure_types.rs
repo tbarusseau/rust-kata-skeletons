c4eb5bcd6bc38447291f1356354ccccf39095d60
ludovic geoffroy <ludovic.geoffroy@thetribe.io>
2020-11-17 12:00:39 +0100

    03 - basic data structure types

--- src/bin/03_basic_data_structure_types.rs
+++ src/bin/03_basic_data_structure_types.rs
@@ -18,8 +18,48 @@
 // - InternalServerError*
 // Values annotated with a * must contain data that describe the error in itself
 
-struct _Error {}
+enum ErrorKind {
+    BadRequest,
+    Unauthorized,
+    DatabaseError(String),
+    InternalServerError(String),
+}
 
-enum _ErrorKind {}
+struct Error {
+    kind: ErrorKind,
+    context: String,
+    status_code: i32,
+}
 
-fn main() {}
+fn main() {
+    let bad_request = Error {
+        kind: ErrorKind::BadRequest,
+        context: String::from("Vous savez pas remplir un formulaire"),
+        status_code: 400,
+    };
+
+    let unauthorized = Error {
+        kind: ErrorKind::Unauthorized,
+        context: String::from("Vous devez passer votre token d'authentification"),
+        status_code: 401,
+    };
+
+    let db_error = Error {
+        kind: ErrorKind::DatabaseError(String::from("Unique constraint error")),
+        context: String::from("Erreur d'accès à la base de données"),
+        status_code: 502,
+    };
+
+    let server_error = Error {
+        kind: ErrorKind::InternalServerError(String::from("Le webmestre ne sait pas coder")),
+        context: String::from("Erreur lors de la communication avec l'api"),
+        status_code: 500,
+    };
+
+    for error in [bad_request, unauthorized, db_error, server_error].iter() {
+        match &error.kind {
+            ErrorKind::BadRequest | ErrorKind::Unauthorized => println!("{} - {}", error.status_code, error.context),
+            ErrorKind::DatabaseError(msg) | ErrorKind::InternalServerError(msg) => println!("{} - {}: {}", error.status_code, error.context, msg),
+        }
+    }
+}
