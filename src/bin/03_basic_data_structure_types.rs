6d088d778ad2a200d9e2aa8c16c5ca636b4806bd
Pierre Lavalley <pierre.lavalley@thetribe.io>
2020-11-17 12:00:02 +0100

    Exo 3

--- src/bin/03_basic_data_structure_types.rs
+++ src/bin/03_basic_data_structure_types.rs
@@ -18,8 +18,41 @@
 // - InternalServerError*
 // Values annotated with a * must contain data that describe the error in itself
 
-struct _Error {}
+struct Error {
+    error_kind:ErrorKind,
+    context: String,
+    status_code: u32,
+}
 
-enum _ErrorKind {}
+enum ErrorKind {
+    BadRequest,
+    Unauthorized,
+    DatabaseError(String),
+    InternalServerError(String),
+}
 
-fn main() {}
+fn main() {
+    let error = Error {
+        error_kind: ErrorKind::BadRequest,
+        context: String::from("Must provide an id"),
+        status_code: 400,
+    };
+
+    let unauthorized = Error {
+        error_kind: ErrorKind::Unauthorized,
+        context: String::from("You cannot access this data"),
+        status_code: 401,
+    };
+
+    let database_error = Error {
+        error_kind: ErrorKind::DatabaseError(String::from("null of undefined")),
+        context: String::from("Database error"),
+        status_code: 500,
+    };
+
+    let internal_error = Error {
+        error_kind: ErrorKind::InternalServerError(String::from("null of undefined")),
+        context: String::from("Internal Server Error"),
+        status_code: 500,
+    };
+}
