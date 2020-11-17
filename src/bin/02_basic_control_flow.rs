fafdc370289e98a9905ada556ac54218fa3700ba
Pierre Cailleaud <pierre.cailleaud@thetribe.io>
2020-11-17 12:00:54 +0100

    02_basic_control_flow

--- src/bin/02_basic_control_flow.rs
+++ src/bin/02_basic_control_flow.rs
@@ -11,8 +11,42 @@
 // - If the number is neither divisible by 3 nor 5, print the number.
 // When it works, we'll create a FizzBuzzJazz function :D
 
-// fn fizzbuzz() {}
+fn fizzbuzz_if() {
+    for x in 0..100 {
+        if x % 5 == 0 && x % 3 == 0 {
+            println!("{} {}", x, "FizzBuzz")
+        } else if x % 3 == 0 {
+            println!("{} {}", x, "Fizz")
+        } else if x % 5 == 0 {
+            println!("{} {}", x, "Buzz")
+        } else {
+            println!("{}", x)
+        }
+    }
+}
+
+fn fizzbuzz_match() {
+    for x in 0..=100 {
+        match (x % 3, x % 5) {
+            (0, 0) => println!("{} {}", x, "FizzBuzz"),
+            (0, _) => println!("{} {}", x, "Fizz"),
+            (_, 0) => println!("{} {}", x, "Buzz"),
+            _ => println!("{}", x),
+        }
+    }
+}
+fn fizzbuzzjazz() {
+    for x in 0..=100 {
+        match (x % 3, x % 5, x % 7) {
+            (0, 0, 0) => println!("{} {}", x, "FizzBuzzJazz"),
+            (0, _, _) => println!("{} {}", x, "Fizz"),
+            (_, 0, _) => println!("{} {}", x, "Buzz"),
+            (_, _, 0) => println!("{} {}", x, "Jazz"),
+            _ => println!("{}", x),
+        }
+    }
+}
 
 fn main() {
-    // fizzbuzz();
+    fizzbuzzjazz();
 }
