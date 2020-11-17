0ba9f74bda878b79cc8f95fd5c1ef46bdb3b822f
Pierre Cailleaud <pierre.cailleaud@thetribe.io>
2020-11-17 12:01:08 +0100

    03_basic_data_structure_types

--- src/bin/03_basic_data_structure_types.rs
+++ src/bin/03_basic_data_structure_types.rs
@@ -18,8 +18,43 @@
 // - InternalServerError*
 // Values annotated with a * must contain data that describe the error in itself
 
-struct _Error {}
+struct Error {
+  error_kind: ErrorKind,
+  context: String,
+  status_code: i32
+}
 
-enum _ErrorKind {}
+enum ErrorKind {
+  BadRequest,
+  Unauthorized,
+  DatabaseError(String),
+  InternalServerError(String)
+}
 
-fn main() {}
+fn display_error(error: Error) {
+  match error.error_kind {
+    ErrorKind::BadRequest => {
+      println!("It's a BadRequest error ! Context: {}, StatusCode: {}", error.context, error.status_code)
+    }
+    ErrorKind::InternalServerError(description) => {
+      println!("It's an InternalServerError ! Description: {}", description)
+    }
+    _ => println!("It's another error !"),
+  }
+}
+
+fn main() {
+  let bd_error = Error {
+    error_kind: ErrorKind::BadRequest,
+    context: String::from("The context"),
+    status_code: 400
+  };
+  let internal_error = Error {
+    error_kind: ErrorKind::InternalServerError(String::from("Explosion")),
+    context: String::from("The context"),
+    status_code: 500
+  };
+
+  display_error(bd_error);
+  display_error(internal_error);
+}
