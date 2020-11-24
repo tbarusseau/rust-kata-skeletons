c9099a088e68eb0bc9d617f5f12faed30a374558
Thomas BARUSSEAU <thomas.barusseau@thetribe.io>
2020-11-24 19:30:27 +0100

    Add 04 samples

--- src/bin/04_generic_types.rs
+++ src/bin/04_generic_types.rs
@@ -13,9 +13,58 @@
 // And... That's it. We're still lacking important stuff to write
 // interesting code with generic types.
 
-// Exercise: create a function that takes a filename (`String`), removes a
-// suffix (`- Copy`), and returns the filename if the suffix was found, else
-// an error. Yes, we'll return a `Result` :D
+struct _BagOfHolding<T> {
+    content: T,
+}
+
+enum _BagOrOther<T, U>  {
+    Bag(_BagOfHolding<T>),
+    Other(U),
+}
+
+const SUFFIX: &str = "- Copy";
+
+fn suffix_remover_1(filename: &String) -> Result<String, String> {
+    if filename.ends_with(SUFFIX) {
+        Ok(filename.split(SUFFIX).collect())
+    }
+    else {
+        Err(format!("Filename does not end with suffix: `{}`", SUFFIX))
+    }
+}
+
+fn _suffix_remover_2(filename: String) -> Result<String, ()> {
+    match filename.strip_suffix(SUFFIX) {
+        Some(s) => Ok(s.to_string()),
+        None => Err(()),
+    }
+}
+
+fn _suffix_remover_3(filename: String) -> Result<String, ()> {
+    if filename.ends_with(SUFFIX) {
+        let len = filename.chars().count();
+        Ok(filename.chars().take(len - SUFFIX.len()).collect())
+    }
+    else {
+        Err(())
+    }
+}
+
+fn _suffix_remover_4(filename: String) -> Result<String, ()> {
+    if filename.ends_with(SUFFIX) {
+        let len = filename.chars().count();
+        Ok(filename[..len - SUFFIX.len()].to_string())
+    }
+    else {
+        Err(())
+    }
+}
+
+// #[test]
+// fn test_suffix_remover() {
+//     const input: &str = "test- Copy";
+//     const output: &str = "test";
+// }
 
 // Exercise: use the previous function, and print the result after being unwrapped!
 
@@ -24,4 +73,11 @@
 
 // Exercise: use the previous function, and print an error message if the function fails.
 
-fn main() {}
+fn main() -> Result<(), String> {
+    let s = "test".to_string();
+    let result = suffix_remover_1(&s)?;
+    println!("{}", result);
+    println!("{}", s);
+
+    Ok(())
+}
